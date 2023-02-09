4859964646f02a9f9ea0782301801d7244a00eb5
Melaku Demeke <melaku.demeke789@gmail.com>
2023-02-09 09:35:51 +0300

    footer page

---
+++ components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+function Footer(){
+    return (
+        <div className="footer">
+            <a href="#">
+                <i className="fab fa-facebook-square fa-2x"></i>
+            </a>
+            <a href="#">
+                <i className="fab fa-twitter-square fa-2x"></i>
+            </a>
+            <a href="#">
+                <i className="fab fa-instagram fa-2x"></i>
+            </a>
+            <a href="https://github.com/MelakuDemeke">
+                <i className="fab fa-github fa-2x"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Footer;
