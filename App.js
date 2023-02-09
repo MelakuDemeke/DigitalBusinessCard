aceeea398a28a1702b8877493a864d843013d008
Melaku Demeke <melaku.demeke789@gmail.com>
2023-02-09 09:36:50 +0300

    main files

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Info from './components/Info'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+      <Info />
+    </div>
+  )
+}
+
+export default App
